53e5384e073d74c7e38997f47954a13707f33613
PhYdrogen <Fossecruor@gmail.com>
2024-01-22 13:59:12 +0000

    feat: better version

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -22,109 +22,111 @@ static void print_grid(int grid[3][3])
         printf("\n");
     }
 }
+/**
+ * is_zero - Check if pile is composed of zero
+ * @grid: 3x3 grid
+ * Return: 0 composed of zero, 1 there is a number that is not zero
+ */
+int is_zero(int grid[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (grid[i][j] != 0)
+                return 1;
+        }
+    }
+    return 0;
+}
 
 /**
  * is_stable - Check if pile is stable
  * @grid: 3x3 grid
- *
+ * Return: 0 or 1 sucess or fail
  */
 int is_stable(int grid[3][3]) {
-  int gridtmp[3][3] = {
-        {0, 0, 0},
-        {0, 0, 0},
-        {0, 0, 0}
-    };
-
-
   for (int i = 0; i < 3; i++)
   {
     for (int j = 0; j < 3; j++)
     {
         if (grid[i][j] > 3) {
-            if (i == 0 && j == 0) {
-                gridtmp[i+1][j] += 1;
-                gridtmp[i][j+1] += 1;
-            }
-            if (i == 1 && j == 0) {
-                gridtmp[i-1][j] += 1;
-                gridtmp[i+1][j] += 1;
-                gridtmp[i][j+1] += 1;
-            }
-            if (i == 2 && j == 0) {
-                gridtmp[i-1][j] += 1; 
-                gridtmp[i][j+1] += 1; 
-            }
-            if (i == 0 && j == 1) {
-                gridtmp[i][j-1] += 1; 
-                gridtmp[i+1][j] += 1; 
-                gridtmp[i][j+1] += 1; 
-            }
-            if (i == 1 && j == 1) {
-                gridtmp[i+1][j] += 1; 
-                gridtmp[i-1][j] += 1; 
-                gridtmp[i][j+1] += 1; 
-                gridtmp[i][j-1] += 1; 
-            }
-            if (i == 2 && j == 1) {
-                gridtmp[i+1][j] += 1; 
-                gridtmp[i-1][j] += 1; 
-                gridtmp[i][j+1] += 1; 
-                gridtmp[i][j-1] += 1; 
-            }
-            if (i == 0 && j == 2) {
-                gridtmp[i][j-1] += 1; 
-                gridtmp[i-1][j] += 1; 
-            } 
-            if (i == 1 && j == 2) {
-                gridtmp[i-1][j] += 1; 
-                gridtmp[i+1][j] += 1; 
-                gridtmp[i][j-1] += 1; 
-            }
-            if (i == 2 && j == 2) {
-                gridtmp[i-1][j] += 1; 
-                gridtmp[i][j-1] += 1; 
-            }
-            printf("%d ->\n", grid[i][j]);
-            print_grid(gridtmp);
-            printf("\n");
-            grid[i][j] -= 4;
+            return 1;
         }
     }
   }
-  printf("tmp\n");
-  print_grid(gridtmp);
-  printf("grid\n");
-  print_grid(grid);
-  grid = gridtmp;
   return 0; // no issue
 }
 
+
+void shamble(int grid[3][3]) {
+    int gridtmp[3][3] = {
+    {0, 0, 0},
+    {0, 0, 0},
+    {0, 0, 0}
+    };
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (grid[i][j] > 3) {
+                if (i >= 1)
+                    gridtmp[i-1][j] += 1;
+                if (i <= 1)
+                    gridtmp[i+1][j] += 1;
+                if (j >= 1)
+                    gridtmp[i][j-1] += 1;
+                if (j <= 1)
+                    gridtmp[i][j+1] += 1;
+
+            // printf("%d ->\n", grid[i][j]);
+            // print_grid(gridtmp);
+            // printf("\n");
+            grid[i][j] -= 4;
+            }
+        }
+    }
+    for (int i = 0; i < 3; i++) { //grid 1
+        for (int j = 0; j < 3; j++) { //grid 2
+            grid[i][j] = grid[i][j] + gridtmp[i][j];
+        }
+    }   
+//   printf("tmp\n");
+//   print_grid(gridtmp);
+//   printf("grid\n");
+//   print_grid(grid);
+
+}
 /**
  * sandpiles_sum - Calculate the sum of sandpiles
  * @grid1: 3x3 grid
  * @grid2: 3x3 grid
- *
+ * Return: 0 or 1 sucess or fail
  */
 int sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-  int gridtemp[3][3];
+    int add = 0;
+    if (is_zero(grid1) == 0 || is_zero(grid2) == 0)
+        add = 1;
 
-  for (int i = 0; i < 3; i++)
-  { //grid 1
-    for (int j = 0; j < 3; j++)
-    { //grid 2
-      gridtemp[i][j] = grid1[i][j] + grid2[i][j];
+    int gridtemp[3][3];
 
+    for (int i = 0; i < 3; i++)
+    { //grid 1
+        for (int j = 0; j < 3; j++)
+        { //grid 2
+            gridtemp[i][j] = grid1[i][j] + grid2[i][j];
+        }
     }
-  }
-  grid1 = gridtemp;
-
-  printf("=\n");
-  print_grid(grid1);
-
-  is_stable(grid1);
-  printf("=\n");
-  print_grid(grid1);
-
-  return 0;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            grid1[i][j] = gridtemp[i][j];
+        }
+    }
+    if (add == 1) 
+        return 0;
+    while(is_stable(grid1) == 1) {
+        printf("=\n");
+        print_grid(grid1);
+        shamble(grid1);
+    }
+    return 0;
 }
