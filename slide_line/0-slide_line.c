18cbb56780af4181550a1ac8c2b9a1e9a6e2d88d
PhYdrogen <Fossecruor@gmail.com>
2024-01-23 14:03:21 +0000

    i have lost

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -6,39 +6,75 @@
 int slide_line(int *line, size_t size, int direction)
 {
 	int linetmp[size];
+	int cleanline[size];
 	for (size_t j = 0; j <= size; j++)
 		linetmp[j] = 0;
 
+	int z = 0;
+	/* Clean zero in line */
+	for (int f = 0; f < (int)size; f++) {
+		if (line[f] != 0) {
+			cleanline[z] = line[f];
+			z++;
+		}
+	}
+	for (int f = z; f < (int)size; f++)
+		cleanline[f] = 0;
+	
+
 	int x = 0;
 	for (int i = 0; i <= (int)size - 1; i++) {
-
-		if (line[i] != 0 && linetmp[x] == line[i]) {
-			linetmp[x] *= 2;
+		if (cleanline[i] != 0 && cleanline[i] == cleanline[i + 1]) {
+			linetmp[x] = cleanline[i] * 2;
+			printf("tmp: %d\n", linetmp[x]);
 			x++;
 			i++;
-		}
-		if (linetmp[x] != line[i]) {
-			linetmp[x] = line[i];
+		} 
+		else if (cleanline[i] != 0 && cleanline[i] != cleanline[i + 1]) {
+			linetmp[x] = cleanline[i];
+			printf("tmp: %d\n", linetmp[x]);
 			x++;
 		}
-		printf("tmp: %d\n", linetmp[x]);
 
-		// if (line[i] != 0) {
-		// 	linetmp[x] = line[i];
-		// 	printf("line: %d", line[i]);
-
-		// }
 	}
+	// if (direction == 0) { /* Left */
+	// 	for (int i = 0; i <= (int)size; i++) {
+	// 		line[i] = linetmp[i];
+	// 		printf("%d\n", linetmp[i]);
+	// 	}
+
+	// } else { /* Right */
+	// 	for (int k = (int)size - 1; k >= 0; k--) {
+	// 		*(line++) = linetmp[k];
+	// 		printf("%d\n", linetmp[k]);
+	// 	}
+	// }
+	/* count zero at the end */
+	int c = 0;
+	for (int z = (int)size - 1; z > 0; z--) {
+		if (linetmp[z] != 0) {
+			break;
+		}
+		c++;
+	}
+
 	if (direction == 0) { /* Left */
 		for (int i = 0; i <= (int)size; i++) {
 			line[i] = linetmp[i];
-			printf("%d\n", linetmp[i]);
+			// printf("%d\n", linetmp[i]);
 		}
 
 	} else { /* Right */
-		for (int k = (int)size - 1; k >= 0; k--) {
-			*(line++) = linetmp[k];
+		for (int k = (int)size-1; k >= 0; k--) {
+		    if (c < 1)
+		      line[k] = linetmp[k];
+		    if (c > 1)
+			    line[k+1] = linetmp[k];
 			printf("%d\n", linetmp[k]);
+		}
+		if (c > 0) {
+			for (int l = 0; l < c; l++)
+				line[l] = 0;
 		}
 	}
 	return 1;
