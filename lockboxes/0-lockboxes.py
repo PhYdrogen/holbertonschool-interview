199b020760722dda342b1a16e0025b7e25fda6ce
Gabriel <fossecruor@gmail.com>
2024-03-26 11:32:46 +0100

    venv + pycodestyle

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -2,11 +2,15 @@
 """
 Function to check if all boxes in a list can be opened.
 
-:param boxes: List of lists where inner lists represent keys and outer list represents boxes.
+:param boxes: List of lists where inner lists represent keys
+and outer list represents boxes.
 :return: True if all boxes can be opened, False otherwise.
 """
+
+
 def canUnlockAll(boxes: list[list[int]]):
     """ Open all the boxes """
+
     a = []
     for e in boxes[0]:
         a.append(e)
@@ -16,5 +20,3 @@ def canUnlockAll(boxes: list[list[int]]):
             if x not in a and x != 0:
                 a.append(x)
     return 1 + len(a) == len(boxes)
-
-
