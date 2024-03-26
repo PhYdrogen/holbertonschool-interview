f0b5698d8b8e9e1d71af9f92fd28af7a1d65622b
Gabriel <fossecruor@gmail.com>
2024-03-26 12:09:00 +0100

    with ruff

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-
-"""Function to check if all boxes in a list can be opened."""
+"""
+Lockboxes
+"""
 
 
 def canUnlockAll(boxes: list[list[int]]) -> bool:
     """ Open all the boxes """
-
     a = []
-    # Create an array
     for e in boxes[0]:
-        # loop over the first box
-
         a.append(e)
-        # add to the array the element
-
     for elem in a:
-        # loop over the array the existing box
         for x in boxes[elem]:
-            # each key of box
             if x not in a and x != 0:
-                # x not 0 and not already in box
                 a.append(x)
-                # add to array
-    # return true or false
     return 1 + len(a) == len(boxes)
