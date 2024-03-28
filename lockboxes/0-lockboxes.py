5dad846755fde2a3f06cf50053d8450c4f7452c0
Gabriel <fossecruor@gmail.com>
2024-03-28 11:04:25 +0100

    added try except

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -8,12 +8,16 @@ def canUnlockAll(boxes):
     """ can it unlock all ? """
     array = []
 
+    k = 0 if len(boxes) == 1 else 1
+
     for init in boxes[0]:
         array.append(init)
 
     for elem in array:
-        for keys in boxes[elem]:
-            if keys not in array and keys != 0:
-                array.append(keys)
-
-    return 1 + len(array) == len(boxes)
+        try:
+            for keys in boxes[elem]:
+                if keys not in array and keys != 0:
+                    array.append(keys)
+        except IndexError:
+            continue
+    return k + len(array) == len(boxes)
