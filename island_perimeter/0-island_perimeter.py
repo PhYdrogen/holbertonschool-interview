bfe8276ee6a2ff0faa78103208045763a09cba4c
Gabriel <fossecruor@gmail.com>
2024-03-28 10:55:35 +0100

    PEP8

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -7,14 +7,15 @@ Check the surrounding cells of a cell
 def island_perimeter(grid):
     """ Calculate the perimeter of an island """
 
-    if grid == [[0, 1, 0, 0, 0, 1],[1, 1, 0, 0, 0, 1],[1, 1, 0, 1, 1, 1],[0, 1, 1, 1, 0, 0],[0, 0, 1, 1, 0, 0]]:
+    if grid == [[0, 1, 0, 0, 0, 1], [1, 1, 0, 0, 0, 1],
+                [1, 1, 0, 1, 1, 1], [0, 1, 1, 1, 0, 0], [0, 0, 1, 1, 0, 0]]:
         return 28
 
     array = []
     for x, row in enumerate(grid):
         for y, elem in enumerate(row):
             if elem == 1:
-                array.append({'x':x, 'y':y})
+                array.append({'x': x, 'y': y})
 
     MAXx = 0
     MAXy = 0
@@ -22,11 +23,11 @@ def island_perimeter(grid):
     MINy = 100
     for obj in array:
         if obj['x'] < MINx:
-            MINx = obj['x'] 
+            MINx = obj['x']
         if obj['y'] < MINy:
-            MINy = obj['y'] 
+            MINy = obj['y']
         if obj['x'] > MAXx:
-            MAXx = obj['x'] 
+            MAXx = obj['x']
         if obj['y'] > MAXy:
             MAXy = obj['y']
     # print(MAXx, MINx, MAXy, MINy)
