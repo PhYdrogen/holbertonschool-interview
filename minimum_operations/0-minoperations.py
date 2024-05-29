3272c8eb17ac49020520fd5e584aadfa5d7cd0c9
Gabriel <fossecruor@gmail.com>
2024-05-29 10:24:03 +0200

    ok

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -4,26 +4,9 @@ Min Operations
 """
 
 def minOperations(n):
-    h_string = "h"
-    op = 0
-    if (n <= 1):
-        return 0
-
-    for i in range(n):
-        if (i == 1):
-            deux = n / 2
-            trois = n / 3
-            # print(deux, trois)
-            if ((trois).is_integer() and (deux).is_integer() and trois < deux):
-                h_string += h_string
-                # print(h_string)
-
-                op += 1
-        else:
-            h_string = h_string * 2
-            # print(h_string, len(h_string))
-
-            op += 2
-            if (len(h_string) >= n):
-                return op
-    return 0
+    if n % 2 == 1:
+        return n
+    else:
+        copy = n // 2
+        paste = n - copy
+        return copy + paste
