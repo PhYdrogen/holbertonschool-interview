cf7a07b33cf9ea0a00a32ffcef9a6163f943ede8
Gabriel <fossecruor@gmail.com>
2024-05-29 10:47:47 +0200

    blc

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -3,17 +3,17 @@
 Min Operations
 """
 
+
 def minOperations(n):
-    if n <= 1:
-        return 0
+    ans = 0
+    i = 2
+    while i*i<=n:
+        if n%i == 0: 
+            ans+=i
+            n/=i
+        else:
+            i+=1
+    if n!=1:
+        ans+=n
 
-    op = 0
-    d = 2
-
-    while op <= n:
-        while n % d == 0:
-            op += d
-            n //= d
-        d += 1
-
-    return op
+    return(int(ans))
