021531e0cd30ed343a926cdc0767718f271ca819
Gabriel <fossecruor@gmail.com>
2024-05-29 10:49:42 +0200

    fix

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -5,15 +5,18 @@ Min Operations
 
 
 def minOperations(n):
+    if (n <= 1):
+        return 0
+
     ans = 0
     i = 2
-    while i*i<=n:
-        if n%i == 0: 
-            ans+=i
-            n/=i
+    while i * i <= n:
+        if n % i == 0:
+            ans += i
+            n /= i
         else:
-            i+=1
-    if n!=1:
-        ans+=n
+            i += 1
+    if n != 1:
+        ans += n
 
     return(int(ans))
