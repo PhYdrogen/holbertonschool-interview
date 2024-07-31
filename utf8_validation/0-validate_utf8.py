2bbb149ccdc78359e1d7cf6d0969b238cfd1146c
Gabriel <fossecruor@gmail.com>
2024-07-31 12:51:48 +0100

    magnifique

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -14,28 +14,34 @@ def validUTF8(data) -> bool:
                 return False
         return True
 
-    f = data[0]
-    f = bin(f)[2:]
-    try:
-        f = f.split('0')[0]
-    except IndexError:
-        return False
-    # ok(data)
-    # print(data[0])
-    if f.count('1') - 1 < 1 or f.count('1') > 4:
-        return False
-    for i in range(f.count('1') - 1):
+    for i in range(len(data)):
+        f = data[i]
+        f = bin(f)[2:]
+
         try:
-            d = bin(data[i+1])[2:]
+            f = f.split('0')[0]
         except IndexError:
             return False
-        d = d.zfill(8)
-        # print(d, i)
-        try:
-            if d.index('10') != 0 and i == f.count('1'):
-                return False
-        except ValueError:
+        ##
+        # ok(data)
+        # print(data[0])
+        ##
+        if f.count('1') - 1 < 1 or f.count('1') > 4:
             return False
+        for x in range(f.count('1') - 1):
+            try:
+                d = bin(data[x+1])[2:]
+            except IndexError:
+                return False
+            d = d.zfill(8)
+            ##
+            # print(d, x)
+            ##
+            try:
+                if d.index('10') != 0 and i == f.count('1'):
+                    return False
+            except ValueError:
+                return False
     return True
 
 
