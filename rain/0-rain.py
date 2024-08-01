f6fbace535a432478dcb59a2f38a930040c0a996
Gabriel <fossecruor@gmail.com>
2024-08-01 09:33:44 +0100

    add rain

---
+++ rain/0-rain.py
@@ -0,0 +1,23 @@
+def rain(walls):
+    if not walls:
+        return 0
+
+    n = len(walls)
+    left_max = [0] * n
+    right_max = [0] * n
+    
+    left_max[0] = walls[0]
+    for i in range(1, n):
+        left_max[i] = max(left_max[i-1], walls[i])
+    
+    right_max[n-1] = walls[n-1]
+    for i in range(n-2, -1, -1):
+        right_max[i] = max(right_max[i+1], walls[i])
+    
+    total_water = 0
+    for i in range(n):
+        water_level = min(left_max[i], right_max[i])
+        if water_level > walls[i]:
+            total_water += water_level - walls[i]
+    
+    return total_water
