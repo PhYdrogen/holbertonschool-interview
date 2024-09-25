534699eadd52834c42e9bb069261263b9056d5f6
Gabriel <fossecruor@gmail.com>
2024-09-25 14:03:36 +0200

    add fn

--- heap_extract/0-heap_extract.c
+++ heap_extract/0-heap_extract.c
@@ -2,6 +2,83 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * heap_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ *
+ * Return: Size of the tree
+ *         0 if tree is NULL
+ */
+size_t heap_size(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (0);
+    return (1 + heap_size(tree->left) + heap_size(tree->right));
+}
+
+/**
+ * get_last_node - gets the last level-order node in a heap
+ * @root: pointer to the root of the heap
+ *
+ * Return: pointer to the last node
+ */
+heap_t *get_last_node(heap_t *root)
+{
+    size_t size;
+    unsigned int bit;
+    heap_t *node;
+
+    if (!root)
+        return (NULL);
+
+    size = heap_size(root);
+    for (bit = 1; !(bit & size); bit <<= 1)
+        ;
+    bit >>= 1;
+
+    for (node = root; bit > 1; )
+    {
+        if (size & bit)
+            node = node->right;
+        else
+            node = node->left;
+        bit >>= 1;
+    }
+
+    return (node);
+}
+
+/**
+ * heapify_down - restores the Max Heap property of a binary heap
+ * @root: pointer to the root of the heap
+ */
+void heapify_down(heap_t *root)
+{
+    heap_t *node = root, *child;
+    int temp;
+
+    while (1)
+    {
+        if (!node->left)
+            break;
+        
+        if (!node->right || node->left->n > node->right->n)
+            child = node->left;
+        else
+            child = node->right;
+
+        if (node->n >= child->n)
+            break;
+
+        temp = node->n;
+        node->n = child->n;
+        child->n = temp;
+
+        node = child;
+    }
+}
+
+
 int heap_extract(heap_t **root)
 {
     int value;
