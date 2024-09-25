76ee9bb7efd83f5e5e14002a264a452b3ab29fbb
Gabriel <fossecruor@gmail.com>
2024-09-25 14:42:08 +0200

    thanks

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -3,10 +3,12 @@
 import typing
 
 def makeChange(coins: typing.List[str], total: int):
-    change = 0
-    for money in coins:
-        if (change + money < total):
-            change += money
-    if change == total:
+    """ Return the minimum number of coins to reach the total """
+    if total <= 0:
         return 0
-    return -1
+    dp = [total + 1] * (total + 1)
+    dp[0] = 0
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[total] if dp[total] != total + 1 else -1
