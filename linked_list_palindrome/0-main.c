b41a14ae7a44352544c7bbf7fbfa099909f7eaac
Gabriel <fossecruor@gmail.com>
2024-09-25 10:44:30 +0200

    init dir

--- linked_list_palindrome/0-main.c
+++ linked_list_palindrome/0-main.c
@@ -3,7 +3,7 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - create empty list and check if it is a palindrome
  *
  * Return: Always 0.
  */
@@ -12,17 +12,6 @@ int main(void)
     listint_t *head;
 
     head = NULL;
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 50);
-    add_nodeint_end(&head, 972);
-    add_nodeint_end(&head, 17);
-    add_nodeint_end(&head, 1);
-    print_listint(head);
 
     if (is_palindrome(&head) == 1)
         printf("Linked list is a palindrome\n");
