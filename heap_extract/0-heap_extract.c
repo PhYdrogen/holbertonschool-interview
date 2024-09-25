b7ab586fa599be19075852605203126c7e6857d7
Gabriel <fossecruor@gmail.com>
2024-09-25 14:01:12 +0200

    add content

--- heap_extract/0-heap_extract.c
+++ heap_extract/0-heap_extract.c
@@ -2,8 +2,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int heap_extract(heap_t **root) {
-    heap_t* cursor = *root;
-    printf("%d", cursor->n);
-    return 0;
+int heap_extract(heap_t **root)
+{
+    int value;
+    heap_t *last_node, *heap_root;
+
+    if (!root || !*root)
+        return (0);
+
+    heap_root = *root;
+    value = heap_root->n;
+    last_node = get_last_node(heap_root);
+
+    if (last_node == heap_root)
+    {
+        *root = NULL;
+        free(heap_root);
+        return (value);
+    }
+
+    heap_root->n = last_node->n;
+
+    if (last_node->parent->left == last_node)
+        last_node->parent->left = NULL;
+    else
+        last_node->parent->right = NULL;
+
+    free(last_node);
+    heapify_down(heap_root);
+
+    return (value);
 }
