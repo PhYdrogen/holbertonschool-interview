eb89617ddfe4ab5b981d345aadaf345975c72762
Gabriel <fossecruor@gmail.com>
2024-09-25 14:08:08 +0200

    merci val

--- heap_extract/binary_trees.h
+++ heap_extract/binary_trees.h
@@ -1,7 +1,5 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
-
-#include <stddef.h>
+#ifndef BINARY_TREES
+#define BINARY_TREES
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,16 +9,19 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+};
 
-void binary_tree_print(const binary_tree_t *);
+typedef struct binary_tree_s binary_tree_t;
+
 typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *tree);
 int heap_extract(heap_t **root);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* BINARY_TREES */
