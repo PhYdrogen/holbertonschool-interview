c00474bf11549c1fabf32d3fccb14066929fe7c9
Gabriel <fossecruor@gmail.com>
2025-01-27 14:23:15 +0100

    if main

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
+"""
+this file will be combined with
+generator to make a resumed of
+incoming request
+"""
 import sys
 
-"""
-this file will be combined with generator
-to make a resumed of incoming request
-"""
+if __name__ == '__main__':
+    fileSize: int = 0
+    status: str = ""
+    d: dict[str, int] = {}
+    nbLine: int = 0
 
-fileSize: int = 0
-status: str = ""
-d: dict[str, int] = {}
-nbLine: int = 0
+    for line in sys.stdin:
+        inputSplit = line.split(" ")
+        n = inputSplit.pop()[:-2]
+        fileSize += int(n)
+        status = inputSplit.pop()
+        value = d.get(status)
+        if value:
+            d[status] = value + 1
+        else:
+            d[status] = 1
 
-for line in sys.stdin:
-    inputSplit = line.split(" ")
-    fileSize += int(inputSplit.pop()[:-2])
-    status = inputSplit.pop()
-    value = d.get(status)
-    if value:
-        d[status] = value + 1
-    else:
-        d[status] = 1
+        nbLine += 1
 
-    nbLine += 1
-
-    if nbLine % 10 == 0:
-        print(f"File size: {fileSize}")
-        arrayKeys: [str] = []
-        for k in d.keys():
-            arrayKeys.append(k)
-        arrayKeys.sort()
-        for keys in arrayKeys:
-            print(f"{keys}:{d[keys]}")
+        if nbLine % 10 == 0:
+            print(f"File size: {fileSize}")
+            arrayKeys: [str] = []
+            for k in d.keys():
+                arrayKeys.append(k)
+            arrayKeys.sort()
+            for keys in arrayKeys:
+                print(f"{keys}:{d[keys]}")
